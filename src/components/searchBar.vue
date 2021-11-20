48829b223628ec9c2bc643f9734845b6d0731f83
BH02 <956750724@qq.com>
2021-11-20 20:06:45 +0800

    Create searchBar.vue

---
+++ src/components/searchBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="searchBar"></div>
+</template>
+<script>
+
+export default ({
+    name:'searchBar'
+})
+</script>
+
+<style scoped>
+.searchBar{
+    width: 30%;
+    height: 80%;
+    background-color: red;
+    border-radius: 1vw;
+    margin-left: 4vw;
+}
+</style>
