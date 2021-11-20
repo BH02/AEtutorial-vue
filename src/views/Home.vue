f4b54001a6a2913aef69ddc0e7d4772f9b80defd
BH02 <956750724@qq.com>
2021-11-20 15:11:45 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="home">
     home
-    <HelloWorld msg='innerMsg'/>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  },
-  data(){
-    return{
-    }
-  },
-  methods:{
-
-  }
 }
 </script>
+<style scoped>
+
+</style>
