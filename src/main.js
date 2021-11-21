1fcad7730b15a62889cdd255cbf112ee6c101f1c
BH02 <956750724@qq.com>
2021-11-21 21:13:39 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'viewerjs/dist/viewer.css'
+import VueViewer from 'v-viewer'
 
+Vue.use(VueViewer)
 Vue.config.productionTip = false
 
 new Vue({
