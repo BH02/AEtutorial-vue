7a275f620389dd324290b804b946a29a820461e6
BH02 <956750724@qq.com>
2021-11-23 19:44:02 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,19 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/selectArticle',
+    name: 'selectArticle',
+    component: () => import('../views/selectArticle.vue')
+  },
+  {
+    path: '/showArticle',
+    name: 'showArticle',
+    component: () => import('../views/showArticle.vue')
   }
 ]
 
