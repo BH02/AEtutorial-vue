e12a597c20f246c68ec2ebf3c1d82c52d1537229
BH02 <956750724@qq.com>
2021-11-23 19:43:57 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import 'viewerjs/dist/viewer.css'
 import VueViewer from 'v-viewer'
+import store from './store'
 
 Vue.use(VueViewer)
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount('#app')
