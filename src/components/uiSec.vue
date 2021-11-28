e4badfd055a25bf58939390fbbc60e28ae0b6a38
BH02 <956750724@qq.com>
2021-11-28 09:40:32 +0800

    Create uiSec.vue

---
+++ src/components/uiSec.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="box">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:'uiSec',
+    props:[
+        'msg'
+    ]
+}
+</script>
+
+<style scoped>
+.box{
+    width: 100%;
+    height: 70px;
+    border-radius: 1vw;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    border-bottom: 1px solid rgba(255, 255, 255, 20%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 3vh;
+    color: #fff8;
+}
+</style>
